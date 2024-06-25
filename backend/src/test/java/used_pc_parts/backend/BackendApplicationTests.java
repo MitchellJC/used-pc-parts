02e23c049688b80723fd8276e5e23b011dbe055c
Mitchell Clark <mitchellclark447@gmail.com>
2024-06-25 22:00:07 +1000

    Added initial backend files

---
+++ backend/src/test/java/used_pc_parts/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package used_pc_parts.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
