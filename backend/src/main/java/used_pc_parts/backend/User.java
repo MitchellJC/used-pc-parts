30e30ecd427cf0885ca58445df92a88794015b5b
Mitchell Clark <mitchellclark447@gmail.com>
2024-06-26 09:40:22 +1000

    Created basic fields for user

--- backend/src/main/java/used_pc_parts/backend/User.java
+++ backend/src/main/java/used_pc_parts/backend/User.java
@@ -1,2 +1,26 @@
-package used_pc_parts.backend;public class User {
+package used_pc_parts.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    protected User() {
+    }
+
+    public User(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
