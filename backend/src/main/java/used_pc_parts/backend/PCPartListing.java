bd6bb0d6030a3c8c82eaa75a87d78a9d189d2af2
Mitchell Clark <mitchellclark447@gmail.com>
2024-06-26 13:43:49 +1000

    Added basic part listing fields

--- backend/src/main/java/used_pc_parts/backend/PCPartListing.java
+++ backend/src/main/java/used_pc_parts/backend/PCPartListing.java
@@ -12,14 +12,87 @@ public class PCPartListing {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  private String sellerId;
   private String name;
-
   private String description;
+  private String images;
+  private PCPartCondition condition;
 
-  protected PCPart() {}
+  private int quantity;
+  private float price;
 
-  public PCPart(String name, String description) {
+  protected PCPartListing() {}
+
+  public PCPartListing(
+      String sellerId,
+      String name,
+      String description,
+      String images,
+      PCPartCondition condition,
+      int quantity,
+      float price) {
+    this.sellerId = sellerId;
     this.name = name;
     this.description = description;
+    this.images = images;
+    this.condition = condition;
+    this.quantity = quantity;
+    this.price = price;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getSellerId() {
+    return sellerId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getImages() {
+    return images;
+  }
+
+  public PCPartCondition getCondition() {
+    return condition;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public float getPrice() {
+    return price;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setImages(String images) {
+    this.images = images;
+  }
+
+  public void setCondition(PCPartCondition condition) {
+    this.condition = condition;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public void setPrice(float price) {
+    this.price = price;
   }
 }
