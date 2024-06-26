25241eada96c2d10d49c69f209f3727e8404a98a
Mitchell Clark <mitchellclark447@gmail.com>
2024-06-26 13:58:34 +1000

    Added boilerplate code

--- backend/src/main/java/used_pc_parts/backend/ListingRepository.java
+++ backend/src/main/java/used_pc_parts/backend/ListingRepository.java
@@ -1,2 +1,5 @@
-package used_pc_parts.backend;public interface ListingRepository {
-}
+package used_pc_parts.backend;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ListingRepository extends CrudRepository<PCPartListing, Long> {}
