266ce2a25121645179b84e14b9723341515f2892
Mitchell Clark <mitchellclark447@gmail.com>
2024-06-27 12:16:14 +1000

    .

--- backend/src/main/java/used_pc_parts/backend/MainController.java
+++ backend/src/main/java/used_pc_parts/backend/MainController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
 
 /** The main controller for the backend. */
