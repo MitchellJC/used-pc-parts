1cb25b141803efffab68d6e3b2233ae6a39365cd
Mitchell Clark <mitchellclark447@gmail.com>
2024-06-28 14:11:48 +1000

    Added security config and dependencies

--- backend/src/main/java/used_pc_parts/backend/SecurityConfig.java
+++ backend/src/main/java/used_pc_parts/backend/SecurityConfig.java
@@ -1,2 +1,24 @@
-package used_pc_parts.backend;public class SecurityConfig {
+package used_pc_parts.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/home", "/greeting"))
+        .formLogin((form) -> form.loginPage("/login").permitAll())
+        .logout(LogoutConfigurer::permitAll);
+    return http.build();
+  }
 }
