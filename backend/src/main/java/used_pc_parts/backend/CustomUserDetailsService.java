5e9b40c9a5708f2d1ce2897590cb75197635d7be
Mitchell Clark <mitchellclark447@gmail.com>
2024-07-01 21:35:45 +1000

    Admin login working with custom user details service

---
+++ backend/src/main/java/used_pc_parts/backend/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package used_pc_parts.backend;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+  @Autowired private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    if (Objects.equals(username, "admin")) {
+      return org.springframework.security.core.userdetails.User.builder()
+          .username("admin")
+          .password(encoder.encode("password"))
+          .authorities("USER", "ADMIN")
+          .build();
+    }
+
+    Optional<User> user = userRepository.findByEmail(username);
+    if (user.isEmpty()) {
+      throw new UsernameNotFoundException("Username not found.");
+    }
+
+    return org.springframework.security.core.userdetails.User.builder()
+        .username(user.get().getEmail())
+        .password(user.get().getHashedPassword())
+        .passwordEncoder(encoder::encode)
+        .authorities("USER")
+        .build();
+  }
+}
--- backend/src/main/java/used_pc_parts/backend/SecurityConfig.java
+++ backend/src/main/java/used_pc_parts/backend/SecurityConfig.java
@@ -3,6 +3,7 @@ package used_pc_parts.backend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,9 +11,16 @@ import org.springframework.security.config.annotation.web.configurers.LogoutConf
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 @EnableWebSecurity
@@ -33,15 +41,25 @@ public class SecurityConfig {
   }
 
   @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user =
-        // TODO Should not use default password encoder in prod
-        User.withDefaultPasswordEncoder()
-            .username("mail")
-            .password("password")
-            .roles("USER")
-            .build();
-
-    return new InMemoryUserDetailsManager(user);
+  public PasswordEncoder encoder() {
+    return new BCryptPasswordEncoder();
   }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new CustomUserDetailsService();
+  }
+
+  //  @Bean
+  //  public UserDetailsService userDetailsService() {
+  //    UserDetails user =
+  //        // TODO Should not use default password encoder in prod
+  //        User.withDefaultPasswordEncoder()
+  //            .username("mail")
+  //            .password("password")
+  //            .roles("USER")
+  //            .build();
+  //
+  //    return new InMemoryUserDetailsManager(user);
+  //  }
 }
