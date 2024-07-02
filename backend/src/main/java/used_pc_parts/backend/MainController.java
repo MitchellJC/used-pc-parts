5659882516309aeb3fb7634cfdd6f0a6d4cfa81f
Mitchell Clark <mitchellclark447@gmail.com>
2024-07-02 11:30:48 +1000

    cleaned up old imports

--- backend/src/main/java/used_pc_parts/backend/MainController.java
+++ backend/src/main/java/used_pc_parts/backend/MainController.java
@@ -1,18 +1,10 @@
 package used_pc_parts.backend;
 
-import java.security.SecureRandom;
-import java.util.Base64;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.server.ResponseStatusException;
 
 /** The main controller for the backend. */
 @Controller
