df745cf95a0f2aff1532db9dc6cba570b8a5ba9d
Mitchell Clark <mitchellclark447@gmail.com>
2024-07-03 11:34:59 +1000

    Moved get all sales resource to sale controller

--- backend/src/main/java/used_pc_parts/backend/sale/SaleController.java
+++ backend/src/main/java/used_pc_parts/backend/sale/SaleController.java
@@ -1,20 +1,18 @@
-package used_pc_parts.backend;
+package used_pc_parts.backend.sale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import used_pc_parts.backend.sale.Sale;
-import used_pc_parts.backend.sale.SaleRepository;
 
-/** The main controller for the backend. */
 @Controller
-public class MainController {
+@RequestMapping(path = "/sale")
+public class SaleController {
   @Autowired private SaleRepository saleRepository;
 
-  @RequestMapping(path = "/greeting")
-  public @ResponseBody String greeting() {
-    return "Hello world!";
+  @GetMapping(path = "/all")
+  public @ResponseBody Iterable<Sale> getAllSales() {
+    return saleRepository.findAll();
   }
 }
