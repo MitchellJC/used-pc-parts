6a5aa68f8e70aced5088f27fc72959fbb031c520
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2024-07-19 15:55:18 +1000

    Login page adjustments for smaller screens

--- frontend/src/app/LoginPage.tsx
+++ frontend/src/app/LoginPage.tsx
@@ -1,6 +1,6 @@
 function LoginPage(): JSX.Element {
   return (
-    <div className="mt-10 mx-auto p-3 min-w-96 bg-slate-800">
+    <div className="mt-10 mx-auto p-3 w-96 max-w-full bg-slate-800">
       <h2 className=" text-center">Please Login</h2>
       <LoginForm></LoginForm>
     </div>
@@ -9,8 +9,8 @@ function LoginPage(): JSX.Element {
 
 function LoginForm() {
   let csrfCookie: string;
-  const loginLabelClass: string = "mt-2 mw-100 min-w-96";
-  const loginInputClass: string = "text-black min-w-96";
+  const loginLabelClass: string = "mt-2 w-prose max-w-full w-prose";
+  const loginInputClass: string = "text-black w-prose max-w-full p-1";
 
   // // Get CSRF cookie
   // let csrfResponse: Response = await fetch("http://127.0.0.1:8080/user/login", {
