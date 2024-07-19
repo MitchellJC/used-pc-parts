f5946daf268f832f07de6904710c268e0561b4b7
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2024-07-19 12:46:17 +1000

    Created Footer component

---
+++ frontend/src/app/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer(): JSX.Element {
+  return (
+    <footer className="bg-gray-900 text-white p-1 text-center">
+      <p>Made by Mitchell Clark</p>
+    </footer>
+  );
+}
+
+export default Footer;
